b3819dbdfc1ae849baddf5337217c9c0760e5b84
EnzoAbelFernandez <enzoabel1@gmail.com>
2025-05-14 22:24:31 -0300

    migración a atlas

--- backend/server.js
+++ backend/server.js
@@ -9,16 +9,16 @@ const path = require('path');
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/auth', authRoutes);
 app.use('/api/pets', petRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('🟢 Conectado a MongoDB');
-    app.listen(process.env.PORT || 5000, () => console.log(`Servidor en puerto ${process.env.PORT}`));
+    app.listen(PORT, () => console.log(`Servidor en puerto ${process.env.PORT}`));
   })
   .catch((err) => console.error('❌ Error de conexión:', err));
