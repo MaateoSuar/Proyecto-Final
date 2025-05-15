ef7457c33b82adc1ad9f90a151a5760c35541819
EnzoAbelFernandez <enzoabel1@gmail.com>
2025-05-14 21:35:29 -0300

    migracion a atlas y unificacion de scripts de npm

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const petRoutes = require('./routes/pets');
+const path = require('path');
 
 dotenv.config();
 
@@ -11,7 +12,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/auth', authRoutes);
 app.use('/api/pets', petRoutes);
 
